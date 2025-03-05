cb80105322ec41415268579f3e5f3214c1994083
Shrike <Jschwass@outlook.com>
2025-03-05 17:05:34 +1300

    Auto-sync mission updates

---
+++ run_watch_git.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell.exe -ExecutionPolicy Bypass -File "C:\Users\jschw\Documents\Arma 3 - Other Profiles\Shrike\missions\RTG-Mission-Sync\watch_git.ps1" -MissionPath "C:\Users\jschw\Documents\Arma 3 - Other Profiles\Shrike\missions\RTG-Mission-Sync" 
