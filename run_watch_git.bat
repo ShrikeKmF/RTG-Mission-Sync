a3ea7b647af66bd70b5c807a4923b40ac5aefbc8
Shrike <Jschwass@outlook.com>
2025-03-05 17:06:53 +1300

    Revert "Auto-sync mission updates"
    This reverts commit cb80105322ec41415268579f3e5f3214c1994083.

--- run_watch_git.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell.exe -ExecutionPolicy Bypass -File "C:\Users\jschw\Documents\Arma 3 - Other Profiles\Shrike\missions\RTG-Mission-Sync\watch_git.ps1" -MissionPath "C:\Users\jschw\Documents\Arma 3 - Other Profiles\Shrike\missions\RTG-Mission-Sync" 
