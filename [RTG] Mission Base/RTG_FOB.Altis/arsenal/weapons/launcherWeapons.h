e218021f5735431ff2553d305948fa4c5afc3e7a
ShrikeKmF <64348318+ShrikeKmF@users.noreply.github.com>
2025-09-20 17:33:36 +1200

    Auto-sync mission updates

--- [RTG] Mission Base/RTG_FOB.Altis/arsenal/weapons/launcherWeapons.h
+++
@@ -1,3 +0,0 @@
-_MediumLaunchers = ["rtg_MRAWS_olive","rtg_MRAWS_sand", "rtg_pslr1_rpg7"];
-_LightLaunchers = ["rtg_m72a7_lat", "rtg_m136_er", "rtg_m136_mat"];
-_HeavyLaunchers = ["launch_I_Titan_short_F", "launch_B_Titan_short_F"];
--- [RTG] Mission Base/RTG_FOB.Altis/initServer.sqf
+++
@@ -1,2 +0,0 @@
-// Setting Respawn Tickets
-[west, 12] call BIS_fnc_respawnTickets;
